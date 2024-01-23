e718ed63afdd61fa1363dbf3c9fc50b9f991dbaa
kubabartecki <169511@stud.prz.edu.pl>
2024-01-23 19:22:58 +0100

    remove data init in main

--- src/main/java/pl/sk8/trickslist/TrickslistApplication.java
+++ src/main/java/pl/sk8/trickslist/TrickslistApplication.java
@@ -1,18 +1,12 @@
 package pl.sk8.trickslist;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import pl.sk8.trickslist.role.RoleService;
-
 
 @SpringBootApplication
 public class TrickslistApplication {
-
-    @Autowired
-    private RoleService roleService;
 
     public static void main(String[] args) {
         SpringApplication.run(TrickslistApplication.class, args);
@@ -20,8 +14,6 @@ public class TrickslistApplication {
 
     @Bean
     CommandLineRunner commandLineRunner() {
-        // todo check why sometimes it inserts 2 of them and other time second is updating st
-        roleService.saveBasicRoles();
         return args -> {
             System.out.println("Hello (⌐■_■)");
         };
